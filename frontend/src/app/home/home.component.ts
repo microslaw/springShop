48da52ecc69e77e9d2e37d12cfb9bd7982cbf058
microslaw <milosz.grunwald@gmail.com>
2025-01-18 21:58:11 +0100

    add shops component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {ShopsComponent} from '../shops/shops.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  imports: [ShopsComponent],
+  // templateUrl: './home.component.html',
+  template: `
+  <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-shops></app-shops>
+    </section>
+  `,
+  styleUrls: ['./home.component.css'],
+  // styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
